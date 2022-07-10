9a74fe4985b895de5e5e18a43b66547d0cb4e6d9
Santeya <santeya@gmx.net>
2022-07-10 14:11:43 +0100

    Homework V.2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,9 +5,7 @@ function solveEquation(a, b, c) {
 
   let d = (b ** 2) - (4 * a * c);
 
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     arr.push(-b / (2 * a));
   } else if (d > 0) {
     arr.push((-b + Math.sqrt(d)) / (2 * a));
@@ -20,21 +18,11 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  if (!isNaN(percent)) {
-    Number.percent;
-  } else if (isNaN(percent)) {
+ if (isNaN(percent)) {
     return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`)
-  }
-
-  if (!isNaN(contribution)) {
-    Number.contribution;
-  } else if (isNaN(contribution)) {
+  } if (isNaN(contribution)) {
     return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`)
-  }
-
-  if (!isNaN(amount)) {
-    Number.amount;
-  } else if (isNaN(amount)) {
+  } if (isNaN(amount)) {
     return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`)
   }
 
@@ -55,7 +43,7 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   let totalPayment = monthlyPayment * amountTerm;
 
-  totalAmount = +(contribution + totalPayment).toFixed(2);
+  totalAmount = +totalPayment.toFixed(2);
 
   console.log(totalAmount);
 
