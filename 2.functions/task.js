1d49eae8b5c8f1e68fd7bf41c97fd53a52bcd6e7
Santeya <santeya@gmx.net>
2022-07-11 16:07:23 +0100

    Homework 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,50 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  let result;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    result = func(arrOfArr[i]);
+    if (result > max) {
+      max = result;
+    }
+  } return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  arr.sort((a, b) => a - b);
+  let min = arr[0];
+  let max = arr[arr.length - 1];
+  let diff = max - min;
+  return diff;
 }
