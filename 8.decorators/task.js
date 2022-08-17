10bd0309a3a303508e771ff773df3d47ab902c29
Santeya <santeya@gmx.net>
2022-08-17 17:07:52 +0100

    Homework 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
 
+    const objectInCache = cache.find((item) => item['hash'] === hash);
+
+    if (objectInCache) {
+      return ('Из кэша: ' + objectInCache['value']);
+    }
+
+    const value = func(...args);
+    cache.push({ 'hash': hash, 'value': value });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    return ('Вычисляем: ' + value);
+  }
+  return wrapper;
+}
 
 function debounceDecoratorNew(func) {
-  // Ваш код
-}
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  function wrapper(...args) {
+    wrapper.allCount++;
 
-function debounceDecorator2(func) {
-  // Ваш код
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    } else if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }
+  return wrapper;
 }
